41f421affb4bb97c7a02f1f625d4648fcc643987
Oliver Schneider <o.schneider819@gmail.com>
2025-12-05 13:17:00 -0700

    made program with LED matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,69 @@
 #include <Arduino.h>
 #include "Arduino_LED_Matrix.h"
 
-ArduinoLEDMatrix matrix;
+struct Snowflake
+{
+  byte x;
+  byte y;
+};
 
+ArduinoLEDMatrix matrix;
+Snowflake snow1 = {7, 1};
+Snowflake snow2 = {11, 6};
 byte frame[8][12] = {
-    {0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0},
-    {0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0},
-    {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+    {0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}};
 
 void setup()
 {
   matrix.begin();
 }
 
+void drawSnowflake(Snowflake &snow);
+void updateSnowflake(Snowflake &snow);
+void resetSnowflake(Snowflake &snow);
+
 void loop()
 {
+  drawSnowflake(snow1);
+  drawSnowflake(snow2);
   matrix.renderBitmap(frame, 8, 12);
-  delay(1000);
+  updateSnowflake(snow1);
+  updateSnowflake(snow2);
+  delay(100);
+}
+
+void drawSnowflake(Snowflake &snow)
+{
+  if (frame[snow.y][snow.x] == 1)
+  {
+    resetSnowflake(snow);
+  }
+  frame[snow.y][snow.x] = 1;
+}
+
+void updateSnowflake(Snowflake &snow)
+{
+  frame[snow.y][snow.x] = 0;
+
+  if (snow.x == 0)
+  {
+    resetSnowflake(snow);
+  }
+  else
+  {
+    snow.x--;
+  }
+}
+
+void resetSnowflake(Snowflake &snow)
+{
+  snow.x = 11;
+  snow.y = random(8);
 }
